2928b63daecce3326f3a55aefa0bd99ff7779ac5
rintarouchida <ww.stamford@gmail.com>
2022-04-17 16:17:00 +0900

    make_route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import { useState, useCallback } from 'react';
-import {ChildArea} from './components/ChildArea';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
 import './style.css';
 
 export default function App() {
-  const [text, setText] = useState('');
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => {
-    setText(e.target.value);
-  }
-
-  const onClickOpen = () => {
-    setOpen(!open);
-  }
-
-  const onClickClose = useCallback(() => setOpen(false), [open]);
   return (
-    <div className="App">
-      <input value={text} onChange={onChangeText}/><br/>
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose}/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br/>
+        <Link to="/page1">Page1</Link>
+        <br/>
+        <Link to="/page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route  path="/page1">
+          <Page1/>
+        </Route>
+        <Route  path="/page2">
+          <Page2/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
